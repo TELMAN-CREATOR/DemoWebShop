c572f85508d66b8c9760b25e81165751503eda19
Telman Bayram <telmanbayram1@gmail.com>
2025-03-23 23:19:59 +0400

    blshbfv

--- src/DownloadTheOrderHistoryToTheComputer.java
+++ src/DownloadTheOrderHistoryToTheComputer.java
@@ -1,6 +1,9 @@
 import Utilty.BaseDriver;
+import org.junit.Test;
 
 public class DownloadTheOrderHistoryToTheComputer extends BaseDriver {
-
-
+@Test
+public  void Test(){
+    System.out.println( "Hello");
+}
 }
